7eb3f3e64f7e3e6ae6460297e80d0878767b6c1a
filmcoder <dominic.napoleon@gmail.com>
2021-01-02 17:43:32 -0500

    change port to 5000

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const Pool = require('pg').Pool
 const app = express();
-const port = 8000;
+const port = 5000;
 
 app.use(bodyParser.json())
 
--- init_db.sql
+++ init_db.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE twinebot;
+
 CREATE TABLE IF NOT EXISTS local_storage_entries (
    key varchar(100) PRIMARY KEY,
    value jsonb
